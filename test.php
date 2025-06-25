a23cea675c50331d66ccfe9909eb1ab6d0b922f1
kai <cgk0910@gmail.com>
2025-06-25 16:05:02 +0800

    table with js

--- test.php
+++ test.php
@@ -3,5 +3,17 @@
     $name = 'kai';
     $text = "hello $name";
 
+
+    $input = $_POST;
+
+    $opt = $input['opt'];
+    $opt = $input['opt'];
+    $opt = $input['opt'];
+    $opt = $input['opt'];
+    $opt = $input['opt'];
+    $opt = $input['opt'];
+    $opt = $input['opt'];
+    $opt = $input['opt'];
+    $opt = $input['opt'];
     
 ?>
